b9a7ce0a0f1eac35e96cd1e93e267f1fa9be972c
dimitarkolev <dk@almero.bg>
2023-01-31 07:37:10 +0000

    Bump

--- src/main.mjs
+++ src/main.mjs
@@ -1,25 +1,30 @@
-import mikserWebhooks from 'mikser-io-webhooks'
+import useMikserWebhooks from 'mikser-io-webhooks'
+import useMikserSubscribe from 'mikser-io-subscribe'
 
 export default async ({ action }, options) => {  
     if (options.env['MIKSER']) {
         const { logger } = options
-        const webhooks = mikserWebhooks({ url: options.env['MIKSER'], token: options.env['MIKSER_TOKEN'], logger })
+        const mikserWebhooks = useMikserWebhooks({ url: options.env['MIKSER'], token: options.env['MIKSER_TOKEN'], logger })
+        const mikserSubscribe = useMikserSubscribe({ url: options.env['MIKSER'], port: options.env['MIKSER_PORT'], token: options.env['MIKSER_TOKEN'], logger })
         
         action('items.create', async ({ collection, key }) => {
-            await webhooks.created(collection, { id: key })
+            await mikserWebhooks.created(collection, { id: key })
+            mikserSubscribe.created(collection, { id: key })
         })
         action('items.update', async ({ collection, keys }) => {
             for(let key of keys) {
-                await webhooks.updated(collection, { id: key })
+                await mikserWebhooks.updated(collection, { id: key })
+                mikserSubscribe.updated(collection, { id: key })
             }
         })
         action('items.delete', async ({ collection, keys }) => {
             for(let key of keys) {
-                await webhooks.deleted(collection, { id: key })
+                await mikserWebhooks.deleted(collection, { id: key })
+                mikserSubscribe.deleted(collection, { id: key })
             }
         })
         action('server.start', async () => {
-            await webhooks.trigger(options.env['MIKSER_URI'] || options.env['PUBLIC_URL'])
+            await mikserWebhooks.trigger(options.env['MIKSER_URI'] || options.env['PUBLIC_URL'])
         })
     } else {
         logger.error('Mikser not found')
